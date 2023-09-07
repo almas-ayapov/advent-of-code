9e3007274c7600858f02d830c2753537ed8d0eff
Almas Ayapov <palmavosaya@tutanota.com>
2023-09-07 11:29:37 +0600

    solved 2015 day 5 part 1

---
+++ 2015/5/aoc-2015-5-part-1-solution.js
@@ -0,0 +1,59 @@
+const readline = require("node:readline");
+const fs = require("node:fs");
+const path = require("node:path");
+
+const rl = readline.createInterface({
+  input: fs.createReadStream(path.join(__dirname, "aoc-2015-5-input.txt")),
+});
+
+let niceStringsCount = 0;
+
+const ALLOWED_VOWELS = "aeiou";
+const REQUIRED_ALLOWED_VOWEL_COUNT = 3;
+const restrictedStrings = ["ab", "cd", "pq", "xy"];
+
+rl.on("line", (line) => {
+  let allowedVowelCount = 0;
+  let duplicateFound = false;
+  let containsRestrictedStrings = false;
+
+  for (let i = 0; i < line.length; i++) {
+    if (containsRestrictedStrings) break;
+
+    const currentChar = line[i];
+    const nextChar = line[i + 1];
+
+    if (
+      ALLOWED_VOWELS.includes(currentChar) &&
+      allowedVowelCount < REQUIRED_ALLOWED_VOWEL_COUNT
+    ) {
+      allowedVowelCount++;
+    }
+
+    if (!nextChar) continue;
+
+    if (!duplicateFound) {
+      duplicateFound = currentChar === nextChar;
+    }
+
+    if (!containsRestrictedStrings) {
+      containsRestrictedStrings = restrictedStrings.includes(
+        `${currentChar}${nextChar}`
+      );
+    }
+  }
+
+  if (
+    allowedVowelCount >= REQUIRED_ALLOWED_VOWEL_COUNT &&
+    duplicateFound &&
+    !containsRestrictedStrings
+  ) {
+    niceStringsCount++;
+  }
+});
+
+rl.on("close", () =>
+  console.log(
+    `AoC 2015 day 5 part 1: Santa has ${niceStringsCount} nice strings in his text file.`
+  )
+);
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-require("./2015/1/aoc-2015-1-part-1-solution.js");
-require("./2015/1/aoc-2015-1-part-2-solution.js");
-require("./2015/2/aoc-2015-2-part-1-solution.js");
-require("./2015/2/aoc-2015-2-part-2-solution.js");
-require("./2015/3/aoc-2015-3-part-1-solution.js");
-require("./2015/3/aoc-2015-3-part-2-solution.js");
-require("./2015/4/aoc-2015-4-part-1-solution.js");
-require("./2015/4/aoc-2015-4-part-2-solution.js");
+// require("./2015/1/aoc-2015-1-part-1-solution.js");
+// require("./2015/1/aoc-2015-1-part-2-solution.js");
+// require("./2015/2/aoc-2015-2-part-1-solution.js");
+// require("./2015/2/aoc-2015-2-part-2-solution.js");
+// require("./2015/3/aoc-2015-3-part-1-solution.js");
+// require("./2015/3/aoc-2015-3-part-2-solution.js");
+// require("./2015/4/aoc-2015-4-part-1-solution.js");
+// require("./2015/4/aoc-2015-4-part-2-solution.js");
+require("./2015/5/aoc-2015-5-part-1-solution.js");
