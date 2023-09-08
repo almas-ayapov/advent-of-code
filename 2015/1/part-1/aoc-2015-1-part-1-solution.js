bcaaf265667aba4644658faf7d97e42318a7b1ea
Almas Ayapov <palmavosaya@tutanota.com>
2023-09-08 15:18:46 +0600

    refactor file structure

--- 2015/1/part-1/aoc-2015-1-part-1-solution.js
+++ 2015/1/part-1/aoc-2015-1-part-1-solution.js
@@ -3,9 +3,12 @@ const path = require("node:path");
 
 let currentFloor = 0;
 
-const rs = fs.createReadStream(path.join(__dirname, "aoc-2015-1-input.txt"), {
-  encoding: "utf-8",
-});
+const rs = fs.createReadStream(
+  path.join(__dirname, "../aoc-2015-1-input.txt"),
+  {
+    encoding: "utf-8",
+  }
+);
 
 rs.on("readable", () => {
   let chunk;
---
+++ 2015/1/part-1/index.js
@@ -0,0 +1,1 @@
+require("./aoc-2015-1-part-1-solution.js");
